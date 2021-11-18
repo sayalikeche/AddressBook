90085656665b0039d1c0888c19031ab03839566c
sayalikeche <kechesayali11@gmail.com>
2021-11-18 22:30:32 +0530

    [sayali added Unique Name ]

--- AddressBook/AddressBook/AddressBook.cs
+++ AddressBook/AddressBook/AddressBook.cs
@@ -7,6 +7,7 @@ namespace AddressBookSystem
     class AddressBook
     {
         List<Contact> addressList = new List<Contact>();
+        Dictionary<string, List<Contact>> dict = new Dictionary<string, List<Contact>>();
         public void AddContact(Contact contact) // This Method Will Add the Details of Customer
         {
             addressList.Add(contact);
@@ -24,6 +25,57 @@ namespace AddressBookSystem
                 Console.WriteLine("PhoneNumber : " + contact.PhoneNumber);
                 Console.WriteLine("Email : " + contact.Email);
             }
+        }
+        public void EditContact(string name)
+        {
+            foreach (var contact in addressList)
+            {
+                if (contact.FirstName == name || contact.LastName == name)
+                {
+                    Console.WriteLine("What is Required to be Edited");
+                }
+            }
+        }
+
+        public void DeleteContact(string user) // This Method Will Delete Particular Contact Detail Provided by User
+        {
+            Contact delete = new Contact();
+            foreach (var contact in addressList)
+            {
+                if (contact.FirstName == user || contact.LastName == user)
+                {
+                    addressList.Remove(contact);
+                }
+            }
+        }
+        public void AddUniqueContact(string nam)
+        {
+            foreach (var contact in addressList)
+            {
+                if (addressList.Contains(contact))
+                {
+                    string uniqueName = Console.ReadLine();
+                    dict.Add(uniqueName, addressList);
+                }
+            }
+        }
+        public void DisplayUniqueContacts()
+        {
+            // Maintain Dictionary of Addressbook Name to Addressbook
+            Console.WriteLine("enter name of dictionary to display that contact details");
+            string name = Console.ReadLine().ToLower();
+            foreach (var contacts in dict)
+            {
+                if (contacts.Key == name)
+                {
+                    foreach (var data in contacts.Value)
+                    {
+                        Console.WriteLine("The Contact of " + data.FirstName + " Details are\n:" + data.FirstName + " " + data.LastName + " " + data.Address + " " + data.City + " " + data.State + " " + data.Zip + " " + data.PhoneNumber + " " + data.Email);
+                    }
+                }
+            }
+            Console.WriteLine("Oops UniqueContacts does not exist!! Please create a UniquecontactList");
+            return;
         }
     }
 }
