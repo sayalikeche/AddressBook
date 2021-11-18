864315abc0ef6660f66042ec1f41e116e32f8569
sayalikeche <kechesayali11@gmail.com>
2021-11-18 21:14:14 +0530

    [sayali adding Address Book]

---
+++ AddressBook/AddressBook/AddressBook.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class AddressBook
+    {
+        List<Contact> addressList = new List<Contact>();
+        public void AddContact(Contact contact) // This Method Will Add the Details of Customer
+        {
+            addressList.Add(contact);
+        }
+        public void Display() // This Method wii Display the Details
+        {
+            foreach (var contact in addressList)
+            {
+                Console.WriteLine(contact.FirstName + " " + contact.LastName);
+                Console.WriteLine("Last Name: " + contact.LastName);
+                Console.WriteLine("Address : " + contact.Address);
+                Console.WriteLine("City : " + contact.City);
+                Console.WriteLine("State : " + contact.State);
+                Console.WriteLine("Zip : " + contact.Zip);
+                Console.WriteLine("PhoneNumber : " + contact.PhoneNumber);
+                Console.WriteLine("Email : " + contact.Email);
+            }
+        }
+        public void EditContact(string name)
+        {
+            foreach (var contact in addressList)
+            {
+                if (contact.FirstName == name || contact.LastName == name)
+                {
+                    Console.WriteLine("What is Required to be Edited");
+                }
+            }
+        }
+
+        public void DeleteContact(string user) // This Method Will Delete Particular Contact Detail Provided by User
+        {
+            Contact delete = new Contact();
+            foreach (var contact in addressList)
+            {
+                if (contact.FirstName == user || contact.LastName == user)
+                {
+                    addressList.Remove(contact);
+                }
+            }
+        }
+    }
+}
